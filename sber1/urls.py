58cda5ef68afcd6caddca0f96473096fd1b8b47a
Смирнов Георгий Валерьевич <georgiy@MacBook-Pro-Smirnov.local>
2025-09-01 15:52:06 +0300

    Complete deps: Django/DRF, gunicorn, WhiteNoise, psycopg2, httpx, pydantic v2, pandas, openpyxl

--- sber1/urls.py
+++ sber1/urls.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from core.views_llm import plan_meeting_view
-
 
 from core import views
 from core.views_api import ClientsListAPI
@@ -15,6 +13,7 @@ from core.views_clients import (
 )
 from core.views_geo import HeatmapAPI
 from core.views_geo_homework import HomeWorkAPI
+from core.views_llm import plan_meeting_view
 
 urlpatterns = [
     # Admin
@@ -23,11 +22,9 @@ urlpatterns = [
     # Pages
     path('', views.index, name='index'),
     path('clients/', views.clients_page, name='clients'),
-    # страница загрузки 5 файлов
-    
     path('upload/multi/', views.upload_multi_page, name='upload_multi'),
 
-    # скачивание отдельных шаблонов
+    # Download templates
     path('download/template/cs/', views.download_template_cs, name='download-template-cs'),
     path('download/template/c/', views.download_template_c, name='download-template-c'),
     path('download/template/tr/', views.download_template_tr, name='download-template-tr'),
@@ -38,7 +35,7 @@ urlpatterns = [
     path('clients/table/', clients_table_view, name='clients_table'),
     path('clients/buckets/', buckets_list_api, name='clients_buckets'),
 
-    # Client detail and heatmap
+    # Client detail and heatmap (фикс путей)
     path('clients/<int:pk>/', client_detail_view, name='client-detail'),
     path('clients/<int:pk>/heatmap/', client_heatmap_view, name='client-heatmap'),
 
@@ -46,9 +43,11 @@ urlpatterns = [
     path('api/clients/', ClientsListAPI.as_view(), name='api_clients'),
     path('api/geo/heatmap/', HeatmapAPI.as_view(), name='geo-heatmap'),
     path('api/geo/homework/', HomeWorkAPI.as_view(), name='geo-homework'),
-    path('api/llm/plan_meeting/', plan_meeting_view, name='llm-plan-meeting'),
+
+    # LLM API (имя совпадает с шаблоном)
+    path('api/llm/plan-meeting/', plan_meeting_view, name='plan_meeting'),
 ]
 
-# Статика (при DEBUG=True)
+# Static/media в DEV
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
